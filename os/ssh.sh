2e1662c65de57ae849bf56d7c76838ab55bf3b13
Tonny Luo <luohoufu@hitao.com>
2016-11-04 11:51:15 +0800

    fix ssh & ssl

--- os/ssh.sh
+++ os/ssh.sh
@@ -8,6 +8,10 @@ command_exists() {
     command -v "$@" > /dev/null 2>&1
 }
 
+if [ -f /root/.ssh/synced ]; then
+    exit 0
+fi
+
 echo "gernerate ssh files and copy to all nodes,please wait......"
 if [ ! -f /root/.ssh/id_rsa ]; then
     ssh-keygen -q -t rsa -N "" -f /root/.ssh/id_rsa
@@ -20,19 +24,21 @@ k8s_node_username=`cat $basepath/config/k8s.json |jq '.k8s.username'|sed 's/\"//
 k8s_node_passwd=`cat $basepath/config/k8s.json |jq '.k8s.passwd'|sed 's/\"//g'`
 
 k8s_node_names=`cat $basepath/config/k8s.json |jq '.k8s.nodes[].name'|sed 's/\"//g'`
-k8s_node_ips=`cat $basepath/config/k8s.json |jq '.k8s.nodes[].ip'|sed 's/\"//g'`
 
 arr_k8s_node_names=($(echo $k8s_node_names))
-arr_k8s_node_ips=($(echo $k8s_node_ips))
 
-master_flag=0
-ip_falg=0
-for ((i=0;i<${#arr_k8s_node_ips[@]};i++));do
-    if echo ${arr_k8s_node_names[$i]}|grep -q "master"; then
+for ((i=0;i<${#arr_k8s_node_names[@]};i++));do
+    k8s_node_hostname=${arr_k8s_node_names[$i]}
+    if echo $k8s_node_hostname|grep -q "master"; then
         continue
     fi
-    expect $basepath/os/expect/expect_ssh.sh ${arr_k8s_node_names[$i]} $k8s_node_username $k8s_node_passwd > /dev/null 2>&1
-    expect $basepath/os/expect/expect_ssh.sh ${arr_k8s_node_ips[$i]} $k8s_node_username $k8s_node_passwd > /dev/null 2>&1
+    if [ -f /root/.ssh/known_hosts ]; then
+        if grep -wq "$k8s_node_hostname" /root/.ssh/known_hosts; then
+            continue
+        fi
+    fi
+    expect $basepath/os/expect/expect_ssh.sh $k8s_node_hostname $k8s_node_username $k8s_node_passwd > /dev/null 2>&1
 done
+touch /root/.ssh/synced
 
 echo "......done"
--- os/ssl.sh
+++ os/ssl.sh
@@ -8,6 +8,10 @@ command_exists() {
     command -v "$@" > /dev/null 2>&1
 }
 
+if [ -f /ssl/synced ]; then
+    exit 0
+fi
+
 echo "gernerate ssl files and copy to all nodes,please wait......"
 if [ ! -d /ssl ]; then
     mkdir -p /ssl
@@ -36,18 +40,16 @@ k8s_node_username=`cat $basepath/config/k8s.json |jq '.k8s.username'|sed 's/\"//
 k8s_node_passwd=`cat $basepath/config/k8s.json |jq '.k8s.passwd'|sed 's/\"//g'`
 
 k8s_node_names=`cat $basepath/config/k8s.json |jq '.k8s.nodes[].name'|sed 's/\"//g'`
-k8s_node_ips=`cat $basepath/config/k8s.json |jq '.k8s.nodes[].ip'|sed 's/\"//g'`
 
 arr_k8s_node_names=($(echo $k8s_node_names))
-arr_k8s_node_ips=($(echo $k8s_node_ips))
 
-master_flag=0
-ip_falg=0
-for ((i=0;i<${#arr_k8s_node_ips[@]};i++));do
-    if echo ${arr_k8s_node_names[$i]}|grep -q "master"; then
+for ((i=0;i<${#arr_k8s_node_names[@]};i++));do
+    k8s_node_hostname=${arr_k8s_node_names[$i]}
+    if echo $k8s_node_hostname|grep -q "master"; then
         continue
     fi
-    expect $basepath/os/expect/expect_scp.sh $k8s_node_username ${arr_k8s_node_names[$i]} $k8s_node_passwd > /dev/null 2>&1
+    expect $basepath/os/expect/expect_scp.sh $k8s_node_hostname $k8s_node_username $k8s_node_passwd > /dev/null 2>&1
+    touch /ssl/synced
 done
 
 echo "......done"
