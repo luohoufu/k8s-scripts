e8c84dc9ce9a534efc6d5de37110a7833ab3063f
Tonny Luo <luohoufu@hitao.com>
2016-11-04 14:32:11 +0800

    etcd conf & service

--- etcd/etcd.sh
+++ etcd/etcd.sh
@@ -17,12 +17,10 @@ arr_etcd_node_ips=($(echo $etcd_node_ips))
 for ((i=0;i<${#arr_etcd_node_ips[@]};i++));do
     if ip a |grep -q ${arr_etcd_node_ips[$i]}; then
         etcd_name=${arr_etcd_node_names[$i]}
+        etcd_ip=${arr_etcd_node_ips[$i]}
     fi
 done
 etcd_endpoints=`echo $etcd_node_names $etcd_node_ips|awk  '{for (i = 1; i < NF/2; i++) printf("%s=https://%s:2380,",$i,$(i+NF/2));printf("%s=https://%s:2380",$i,$(i+NF/2))}'`
-echo $etcd_name
-echo $etcd_endpoints
-exit 1
 
 # Create etcd.conf, etcd.service
 user=etcd
@@ -53,27 +51,27 @@ if [ ! -d "$data" ]; then
 fi
 
 # config file
-cat <<EOF >/etc/etcd/etcd.conf
+cat <<EOF >$conf
 # [member]
-ETCD_NAME=default
-ETCD_DATA_DIR="${etcd_data_dir}/default.etcd"
+ETCD_NAME=${etcd_name}
+ETCD_DATA_DIR="${data}/${etcd_name}"
 #ETCD_SNAPSHOT_COUNTER="10000"
 #ETCD_HEARTBEAT_INTERVAL="100"
 #ETCD_ELECTION_TIMEOUT="1000"
-#ETCD_LISTEN_PEER_URLS="http://localhost:2380,http://localhost:7001"
-ETCD_LISTEN_CLIENT_URLS="http://0.0.0.0:2379"
+ETCD_LISTEN_PEER_URLS="https://${etcd_ip}:2380"
+ETCD_LISTEN_CLIENT_URLS="https://${etcd_ip}:2379,https://127.0.0.1:2379"
 #ETCD_MAX_SNAPSHOTS="5"
 #ETCD_MAX_WALS="5"
 #ETCD_CORS=""
 #
 #[cluster]
-#ETCD_INITIAL_ADVERTISE_PEER_URLS="http://localhost:2380,http://localhost:7001"
+ETCD_INITIAL_ADVERTISE_PEER_URLS="https://${etcd_ip}:2380"
 # if you use different ETCD_NAME (e.g. test), 
 # set ETCD_INITIAL_CLUSTER value for this name, i.e. "test=http://..."
-#ETCD_INITIAL_CLUSTER="default=http://localhost:2380,default=http://localhost:7001"
-#ETCD_INITIAL_CLUSTER_STATE="new"
-#ETCD_INITIAL_CLUSTER_TOKEN="etcd-cluster"
-ETCD_ADVERTISE_CLIENT_URLS="http://localhost:2379"
+ETCD_INITIAL_CLUSTER="${etcd_endpoints}"
+ETCD_INITIAL_CLUSTER_STATE="new"
+ETCD_INITIAL_CLUSTER_TOKEN="k8s-etcd-cluster"
+ETCD_ADVERTISE_CLIENT_URLS="https://${etcd_ip}:2379"
 #ETCD_DISCOVERY=""
 #ETCD_DISCOVERY_SRV=""
 #ETCD_DISCOVERY_FALLBACK="proxy"
@@ -83,25 +81,25 @@ ETCD_ADVERTISE_CLIENT_URLS="http://localhost:2379"
 #ETCD_PROXY="off"
 #
 #[security]
-#ETCD_CA_FILE=""
-#ETCD_CERT_FILE=""
-#ETCD_KEY_FILE=""
-#ETCD_PEER_CA_FILE=""
-#ETCD_PEER_CERT_FILE=""
-#ETCD_PEER_KEY_FILE=""   
+ETCD_CA_FILE="/ssl/ca.pem"
+ETCD_CERT_FILE="/ssl/etcd.pem"
+ETCD_KEY_FILE="/ssl/etcd-key.pem"
+ETCD_PEER_CA_FILE="/ssl/ca.pem"
+ETCD_PEER_CERT_FILE="/ssl/etcd.pem"
+ETCD_PEER_KEY_FILE="/ssl/etcd-key.pem"
 EOF
 
-cat <<EOF >//usr/lib/systemd/system/etcd.service
+cat <<EOF >$service
 [Unit]
 Description=Etcd Server
 After=network.target
 
 [Service]
 Type=simple
-WorkingDirectory=${etcd_data_dir}
-EnvironmentFile=-/opt/kubernetes/cfg/etcd.conf
+WorkingDirectory=${data}
+EnvironmentFile=-${conf}
 # set GOMAXPROCS to number of processors
-ExecStart=/bin/bash -c "GOMAXPROCS=\$(nproc) /opt/kubernetes/bin/etcd"
+ExecStart=/bin/bash -c "GOMAXPROCS=\$(nproc) /usr/bin/etcd"
 
 [Install]
 WantedBy=multi-user.target
@@ -117,6 +115,7 @@ else
     echo 'install error!'
 fi
 
+#setting alias
 if ! grep -q "etcdctl" /root/.bashrc ; then
     sed -i "/alias vi/a\alias  etcdctl='etcdctl --ca-file=/ssl/ca.pem --cert-file=/ssl/etcd.pem --key-file=/ssl/etcd-key.pem --endpoints=$etcd_endpoints'" /root/.bashrc
 fi
