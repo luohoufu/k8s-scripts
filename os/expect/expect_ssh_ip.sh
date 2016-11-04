bc96e8e31185340bd765d9642e452aa57ce5e264
Tonny Luo <luohoufu@hitao.com>
2016-11-04 11:30:24 +0800

    fix expect bug

--- os/expect/expect_ssh_ip.sh
+++ os/expect/expect_ssh_ip.sh
@@ -5,7 +5,7 @@ set timeout 30
 set host [lindex $argv 0]
 set username [lindex $argv 1]
 set passwd [lindex $argv 2]
-set identity_file='/root/.ssh/id_rsa.pub'
+set identity_file="/root/.ssh/id_rsa.pub"
 
 spawn ssh-copy-id -i $identity_file $username@$host
 expect {
