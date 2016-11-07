9a576aecc8086715282ce29db20e4b36d2b6c967
Tonny Luo <luohoufu@hitao.com>
2016-11-07 16:33:46 +0800

    change param

--- kubernetes/node/kubelet.sh
+++ kubernetes/node/kubelet.sh
@@ -85,14 +85,16 @@ KUBE_POD_INFRA="--pod-infra-container-image=registry.access.redhat.com/rhel7/pod
 KUBELET_ARGS=""
 EOF
 
-KUBELET_OPTS="   \${KUBE_LOGTOSTDERR}     \\
-                    \${KUBE_LOG_LEVEL}       \\
-                    \${NODE_ADDRESS}         \\
-                    \${NODE_PORT}            \\
-                    \${NODE_HOSTNAME}        \\
-                    \${KUBELET_API_SERVER}   \\
-                    \${KUBE_ALLOW_PRIV}      \\
-                    \${KUBELET_ARGS}"
+KUBELET_OPTS="  \${KUBE_LOGTOSTDERR}     \\
+                \${KUBE_LOG_LEVEL}       \\
+                \${NODE_ADDRESS}         \\
+                \${NODE_PORT}            \\
+                \${NODE_HOSTNAME}        \\
+                \${KUBE_CONFIG}          \\
+                \${KUBE_REQUIRE_CONFIG}  \\
+                \${KUBE_POD_INFRA}       \\
+                \${KUBE_ALLOW_PRIV}      \\
+                \${KUBELET_ARGS}"
 
 cat <<EOF >$service
 [Unit]
