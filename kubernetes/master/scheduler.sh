ebdff7d9b8a6e11ddf05f966b91f83321a25c0b1
Tonny Luo <luohoufu@hitao.com>
2016-11-07 14:45:09 +0800

    fix basepath

--- kubernetes/master/scheduler.sh
+++ kubernetes/master/scheduler.sh
@@ -3,7 +3,7 @@
 
 set -e -o pipefail -o errtrace -o functrace
 
-basepath=$(cd `dirname $0`;cd ..; pwd)
+basepath=$(cd `dirname $0`;cd ../..; pwd)
 command_exists() {
     command -v "$@" > /dev/null 2>&1
 }
