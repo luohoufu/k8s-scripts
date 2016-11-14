c1a062999f1ecfac44b1ad07ccc097d3e7f197c3
Tonny Luo <luohoufu@hitao.com>
2016-11-14 16:37:31 +0800

    fix add-on

--- kubernetes/add-on/heapster/install.sh
+++ kubernetes/add-on/heapster/install.sh
@@ -23,6 +23,7 @@ fi
 
 export PATH=$PATH:$basepath/tools
 json=$basepath/config/k8s.json
+cert_dir=`jq -r '.cert.dir' $json`
 registry_ip=`jq -r '.docker.registry.ip' $json`
 registry_port=`jq -r '.docker.registry.port' $json`
 registry_url=$registry_ip":"$registry_port
@@ -32,6 +33,7 @@ yaml=$basepath/kubernetes/add-on/dashboard/kubernetes-heapster.yaml
 
 # setting apiserver ip address
 sed -i "s/registry_url/$registry_url/g" $yaml
+sed -i "s/cert_dir/$cert_dir/g" $yaml
 
 if [ $(kubectl get po --namespace=kube-system| grep $name |wc -l) -eq 0 ]; then
     kubectl create -f $yaml
